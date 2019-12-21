1511507aec538175b38002def7138b01e3f4fcd1
guoqi <guoqi76327@163.com>
2019-12-21 14:25:06 +0800

    留言展示

--- xiaoxiao_app/xiaoxiao_blogs/src/main/resources/static/js/leave-message.js
+++ xiaoxiao_app/xiaoxiao_blogs/src/main/resources/static/js/leave-message.js
@@ -1,4 +1,10 @@
 $(function () {
+
+
+    /**
+     * 请求我的个人信息
+     */
+    show_me()
     /**
      * 请求留言个数
      */
@@ -10,6 +16,26 @@ $(function () {
     find_all_leave_message(1)
 
 })
+
+
+
+/**
+ * 展示我的个人信息
+ */
+function show_me() {
+    $.ajax("/frontline/users/show_me",{
+        dataType: "json",
+        type: "POST",
+        timeout: 5000,
+        success:(data)=>{
+            $("#userNickname").text(data.data.userNickname)
+            $("#userSignature").text(data.data.userSignature)
+            $("img[name = 'userProfilePhoto']").prop("src",data.data.userProfilePhoto)
+        }
+    })
+}
+
+
 
 
 /**
